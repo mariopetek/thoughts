f127effbd93dca6dddd9df9e48de69cea12c6f1a
Mario Petek <mariopetek0@gmail.com>
2023-12-17 15:09:43 +0100

    Database ssl

--- db/index.ts
+++ db/index.ts
@@ -3,5 +3,5 @@ import pg from 'pg'
 const { Pool } = pg
 
 export const db = new Pool({
-    connectionString: process.env.POSTGRES_URL
+    connectionString: process.env.POSTGRES_URL + '?sslmode=require'
 })
